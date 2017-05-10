284be7e5d79b94f1fdd9f9864889154a6fcd59c1
chiehwenchen <wxyz710329@gmail.com>
2017-05-10 18:05:13 +0800

    connect fb

--- app.py
+++ app.py
@@ -8,10 +8,20 @@ def verify():
 	# Webhook verification
 	if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
 		if not request.args.get("hub.verify_token") == "hello bot":
-			return "Verification token mismatch", 403
+			return "Failed validation. Make sure the validation tokens match.", 403
 		return request.args["hub.challenge"], 200
-	return "Hello world", 200
+	return "Hello", 200
 
+
+@app.route('/', methods = ['POST'])
+def webhook():
+	data = request.get_json()
+	log(data)
+
+def log(message):
+	print(message)
+	sys.stdout.flush()
+	
 
 if __name__ == "__main__":
 	app.run(debug = True, port = 80)
